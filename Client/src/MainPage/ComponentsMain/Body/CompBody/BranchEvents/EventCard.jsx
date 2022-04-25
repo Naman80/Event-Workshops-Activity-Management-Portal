3687fc9557ec52e7bae780c9d4be9d55fac7b72d
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-04-25 23:50:10 +0530

    Add files via upload

--- Client/src/MainPage/ComponentsMain/Body/CompBody/BranchEvents/EventCard.jsx
+++ Client/src/MainPage/ComponentsMain/Body/CompBody/BranchEvents/EventCard.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './EventCard.css';
 const EventCard = (props) => {
   return (
-
      <div className="card">
       <Link to={"/" + props.title}>
-          <img src={props.img} />
+          {/* <img src={props.img} /> */}
           <div className="card-body">
             <h2>{props.title}</h2>
+            <h4>date</h4>
+            <h4>time</h4>
+            <h4>venue</h4>
           </div>
           </Link>
         </div>
-
-
   )
 }
 
